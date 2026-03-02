dc6932b37c1ba06d32ebf7c2c2f99484b5170d0c
SeongYun Cho <rekafekili@gmail.com>
2026-03-02 22:12:28 +0900

    #4.3 Firecrawl Tool

--- job-hunter-agent/main.py
+++ job-hunter-agent/main.py
@@ -4,13 +4,20 @@ dotenv.load_dotenv()
 
 from crewai import Crew, Agent, Task
 from crewai.project import CrewBase, crew, agent, task
+from models import JobList, ChosenJob, RankedJobList
+from tools import web_search_tool
 
 
 @CrewBase
 class JobHunterCrew:
     @agent
     def job_search_agent(self):
-        return Agent(config=self.agents_config["job_search_agent"])
+        return Agent(
+            config=self.agents_config["job_search_agent"],
+            tools=[
+                web_search_tool,
+            ],
+        )
 
     @agent
     def job_matching_agent(self):
@@ -30,15 +37,24 @@ class JobHunterCrew:
 
     @task
     def job_extraction_task(self):
-        return Task(config=self.tasks_config["job_extraction_task"])
+        return Task(
+            config=self.tasks_config["job_extraction_task"],
+            output_pydantic=JobList,
+        )
 
     @task
     def job_matching_task(self):
-        return Task(config=self.tasks_config["job_matching_task"])
+        return Task(
+            config=self.tasks_config["job_matching_task"],
+            output_pydantic=RankedJobList,
+        )
 
     @task
     def job_selection_task(self):
-        return Task(config=self.tasks_config["job_selection_task"])
+        return Task(
+            config=self.tasks_config["job_selection_task"],
+            output=ChosenJob,
+        )
 
     @task
     def resume_rewriting_task(self):
---
+++ job-hunter-agent/tools.py
@@ -0,0 +1,39 @@
+import os, re
+from crewai.tools import tool
+from firecrawl import FirecrawlApp, ScrapeOptions
+
+
+@tool
+def web_search_tool(query: str):
+    app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+    response = app.search(
+        query=query,
+        limit=5,
+        scrape_options=ScrapeOptions(
+            formats=["markdown"],
+        ),
+    )
+
+    if not response.success:
+        return "Error using tool."
+
+    cleaned_chunks = []
+
+    for result in response.data:
+        title = result["title"]
+        url = result["url"]
+        markdown = result["markdown"]
+
+        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+
+        cleaned_chunks.append(
+            {
+                "title": title,
+                "url": url,
+                "markdown": cleaned,
+            }
+        )
+
+    return cleaned_chunks
