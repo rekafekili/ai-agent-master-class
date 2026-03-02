70e3df266840eba64332789fe60db267f57f3c6f
SeongYun Cho <rekafekili@gmail.com>
2026-03-02 16:46:36 +0900

    #3.3 News Reader Tasks and Agents

--- news-reader-agent/main.py
+++ news-reader-agent/main.py
@@ -7,43 +7,35 @@ from crewai.project import CrewBase, agent, task, crew
 from tools import count_letters
 
 
-# IMPORTANT : Role, Goal, Backstory
 @CrewBase
-class TranslatorCrew:
-    @agent  # https://docs.crewai.com/en/concepts/agents
-    def translator_agent(self):
-        return Agent(config=self.agents_config["translator_agent"])
+class NewsReaderCrew:
+    @agent
+    def news_hunter_agent(self):
+        return Agent(config=self.agents_config["news_hunter_agent"])
 
     @agent
-    def counter_agent(self):
-        return Agent(config=self.agents_config["counter_agent"])
+    def summarizer_agent(self):
+        return Agent(config=self.agents_config["summarizer_agent"])
 
-    @task  # https://docs.crewai.com/en/concepts/tasks
-    def translate_task(self):
-        return Task(config=self.tasks_config["translate_task"])
-
-    @task
-    def retranslate_task(self):
-        return Task(config=self.tasks_config["retranslate_task"])
+    @agent
+    def curator_agent(self):
+        return Agent(config=self.agents_config["curator_agent"])
 
     @task
-    def retranslate_task(self):
-        return Task(
-            config=self.tasks_config["counter_task"],
-            tools=[count_letters],
-        )
+    def content_harvesting_task(self):
+        return Task(config=self.tasks_config["content_harvesting_task"])
+
+    @task
+    def summarization_task(self):
+        return Task(config=self.tasks_config["summarization_task"])
+
+    @task
+    def final_report_assembly_task(self):
+        return Task(config=self.tasks_config["final_report_assembly_task"])
 
     @crew
-    def assemble_crew(self):
-        return Crew(
-            agents=self.agents,  # @agent 가 붙은 agent 모음
-            tasks=self.tasks,  # @task 가 붙은 task 모음
-            verbose=True,  # log
-        )
+    def crew(self):
+        return Crew(agents=self.agents, tasks=self.tasks, verbose=True)
 
 
-TranslatorCrew().assemble_crew().kickoff(
-    inputs={
-        "sentence": "I'm Nico and I like to ride my bicycle in Napoli",
-    }
-)
+NewsReaderCrew().crew().kickoff()
