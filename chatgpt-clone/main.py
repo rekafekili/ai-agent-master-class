88fdaf023243ace78df22e347cf9a51297f256f2
SeongYun Cho <rekafekili@gmail.com>
2026-03-04 03:18:42 +0900

    #8.0 Chat UI - for Assignment

---
+++ chatgpt-clone/main.py
@@ -0,0 +1,49 @@
+import dotenv
+
+dotenv.load_dotenv()
+
+import asyncio
+import streamlit as st
+from agents import Agent, Runner, SQLiteSession
+
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="ChatGPT Clone",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+
+agent = st.session_state["agent"]
+
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history",
+        "chat-gpt-clone-memory.db",
+    )
+
+session = st.session_state["session"]
+
+
+async def run_agent(message):
+    stream = Runner.run_streamed(agent, message, session=session)
+
+    async for event in stream.stream_events():
+        if event.type == "raw_response_event":
+            if event.data.type == "response.output_text.delta":
+                with st.chat_message("ai"):
+                    st.write(event.data.delta)
+
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+    asyncio.run(run_agent(prompt))
+
+with st.sidebar:
+    reset = st.button("Reset memory")
+    if reset:  # reset is True
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
