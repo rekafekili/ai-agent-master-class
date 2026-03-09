f7d2a39d88e2d5835f7c761adad8afde39f62950
SeongYun Cho <rekafekili@gmail.com>
2026-03-09 12:34:09 +0900

    Assign9: Life Coach - Image

--- life-coach-agent/main.py
+++ life-coach-agent/main.py
@@ -3,9 +3,10 @@ import dotenv
 dotenv.load_dotenv()
 
 from openai import OpenAI
-import asyncio
+import asyncio, base64
 import streamlit as st
-from agents import Agent, Runner, SQLiteSession, WebSearchTool, FileSearchTool
+from agents import Agent, Runner, WebSearchTool, FileSearchTool, ImageGenerationTool
+from filtered_sqlite_session import FilterSQLiteSession
 
 client = OpenAI()
 
@@ -21,10 +22,13 @@ if "agent" not in st.session_state:
         or negative emotions the user is facing, 
         and to instill them with realistic, positive energy 
         so they can get back on their feet.
+        Note: When a user achieves a goal, congratulate them by creating an image with a congratulatory message.
+        
 
         You have access to the following tools:
             - Web Search Tool: Use this when the user asks a questions that isn't in your training data. Use this tool when the users asks about current or future events, when you think you don't know the answer, try searching for it in the web first.
             - File Search Tool: Use this tool when the user asks a question about facts related to themselves. Or when they ask questions about specific files.
+            - Image Generation Tool: Use this tool when the user achieves the goal or ask to make a picture what he want.
         """,
         tools=[
             WebSearchTool(),
@@ -32,13 +36,22 @@ if "agent" not in st.session_state:
                 vector_store_ids=[VECTOR_STORE_ID],
                 max_num_results=3,  # 파일이 여러개일 경우 상위 3개만 가져옴.
             ),
+            ImageGenerationTool(
+                tool_config={
+                    "type": "image_generation",
+                    "quality": "low",
+                    "output_format": "jpeg",
+                    "moderation": "low",
+                    "partial_images": 1,
+                }
+            ),
         ],
     )
 
 agent = st.session_state["agent"]
 
 if "session" not in st.session_state:
-    st.session_state["session"] = SQLiteSession(
+    st.session_state["session"] = FilterSQLiteSession(
         "chat-history",
         "chat-gpt-clone-memory.db",
     )
@@ -57,12 +70,17 @@ async def paint_history():
                     if message["type"] == "message":
                         st.write(message["content"][0]["text"])
         if "type" in message:
-            if message["type"] == "web_search_call":
+            message_type = message["type"]
+            if message_type == "web_search_call":
                 with st.chat_message("ai"):
                     st.write("🔎 Searched the web...")
-            elif message["type"] == "file_search_call":
+            elif message_type == "file_search_call":
                 with st.chat_message("ai"):
                     st.write("🗂️ Searched the Files...")
+            elif message_type == "image_generation_call":
+                image_url = base64.b64decode(message["result"])
+                with st.chat_message("ai"):
+                    st.image(image_url)
 
 
 asyncio.run(paint_history())
@@ -92,6 +110,15 @@ def update_status(status_container, event):
             "🔎 File Search in Progress...",
             "running",
         ),
+        "response.image_generation_call.generating": (
+            "🎨 Drawing image...",
+            "running",
+        ),
+        "response.image_generation_call.in_progress": (
+            "🎨 Drawing image...",
+            "running",
+        ),
+        "response.completed": (" ", "complete"),
     }
 
     if event in status_messages:
@@ -101,8 +128,9 @@ def update_status(status_container, event):
 
 async def run_agent(message):
     with st.chat_message("ai"):
-        text_placeholder = st.empty()
         status_container = st.status("⏳", expanded=False)
+        text_placeholder = st.empty()
+        image_placeholder = st.empty()
         response = ""
 
         stream = Runner.run_streamed(
@@ -117,12 +145,15 @@ async def run_agent(message):
                 if event.data.type == "response.output_text.delta":
                     response += event.data.delta
                     text_placeholder.write(response)
+                elif event.data.type == "response.image_generation_call.partial_image":
+                    image = base64.b64decode(event.data.partial_image_b64)
+                    image_placeholder.image(image)
 
 
 prompt = st.chat_input(
     "Write a message for your assistant",
     accept_file=True,
-    file_type=["txt"],
+    file_type=["txt", "jpg", "jpeg", "png"],
 )
 
 if prompt:
@@ -140,6 +171,31 @@ if prompt:
                         file_id=uploaded_file.id,
                     )
                     status.update(label="✅ File Uploaded", state="complete")
+        elif file.type.startswith("image/"):
+            with st.status("⏳ Uploading image...") as status:
+                file_bytes = file.getvalue()
+                base64_data = base64.b64encode(file_bytes).decode("utf-8")
+                data_uri = f"data:{file.type};base64,{base64_data}"
+                st.write(data_uri)
+                asyncio.run(
+                    session.add_items(
+                        [
+                            {
+                                "role": "user",
+                                "content": [
+                                    {
+                                        "type": "input_image",
+                                        "detail": "auto",
+                                        "image_url": data_uri,
+                                    }
+                                ],
+                            }
+                        ]
+                    )
+                )
+                status.update(label="✅ Image Uploaded", state="complete")
+            with st.chat_message("human"):
+                st.image(data_uri)
 
     if prompt.text:
         with st.chat_message("human"):
