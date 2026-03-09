fa3d5931c663936c931f35b9c9556c3979341d11
SeongYun Cho <rekafekili@gmail.com>
2026-03-10 05:10:54 +0900

    #8.8 Local MCP Server

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -17,59 +17,11 @@ from agents import (
     HostedMCPTool,  # Call Remote MCP Server
 )
 from filtered_sqlite_session import FilterSQLiteSession
+from agents.mcp.server import MCPServerStdio
 
 client = OpenAI()
 
 VECTOR_STORE_ID = "vs_69a72f33928881918681632893753257"
-
-if "agent" not in st.session_state:
-    st.session_state["agent"] = Agent(
-        name="ChatGPT Clone",
-        instructions="""
-        You are a helpful assistant.
-
-        You have access to the following tools:
-            - Web Search Tool: Use this when the user asks a questions that isn't in your training data. Use this tool when the users asks about current or future events, when you think you don't know the answer, try searching for it in the web first.
-            - File Search Tool: Use this tool when the user asks a question about facts related to themselves. Or when they ask questions about specific files.
-            - Code Interpreter Tool: Use this tool when you need to write and run code to answer the user's question.
-        """,
-        tools=[
-            WebSearchTool(),
-            FileSearchTool(
-                vector_store_ids=[VECTOR_STORE_ID],
-                max_num_results=3,  # 파일이 여러개일 경우 상위 3개만 가져옴.
-            ),
-            ImageGenerationTool(
-                tool_config={
-                    "type": "image_generation",
-                    "quality": "low",
-                    "output_format": "jpeg",
-                    "moderation": "low",
-                    "partial_images": 1,
-                }
-            ),
-            CodeInterpreterTool(
-                tool_config={
-                    "type": "code_interpreter",
-                    "container": {
-                        "type": "auto",
-                        "file_ids": [],  # OpenAI에 업로드 했던 파일의 ID (코드에서 접근할 수 있도록 허용)
-                    },
-                }
-            ),
-            HostedMCPTool(
-                tool_config={
-                    "server_url": "https://mcp.context7.com/mcp",
-                    "type": "mcp",
-                    "server_label": "Context7",
-                    "server_description": "Use this to get the docs from software projects",
-                    "require_approval": "never",
-                }
-            ),
-        ],
-    )
-
-agent = st.session_state["agent"]
 
 if "session" not in st.session_state:
     st.session_state["session"] = FilterSQLiteSession(
@@ -205,36 +157,96 @@ def update_status(status_container, event):
 
 
 async def run_agent(message):
-    with st.chat_message("ai"):
-        status_container = st.status("⏳", expanded=False)
-        text_placeholder = st.empty()
-        image_placeholder = st.empty()
-        code_placeholder = st.empty()
-        response = ""
-        code_response = ""
+    yfinance_server = MCPServerStdio(
+        params={
+            "command": "uvx",
+            "args": ["mcp-yahoo-finance"],
+        },
+        cache_tools_list=True,
+        client_session_timeout_seconds=60,
+    )
 
-        st.session_state["code_placeholder"] = code_placeholder
-        st.session_state["image_placeholder"] = image_placeholder
-        st.session_state["text_placeholder"] = text_placeholder
+    async with yfinance_server:
+        agent = Agent(
+            mcp_servers=[yfinance_server],
+            name="ChatGPT Clone",
+            instructions="""
+            You are a helpful assistant.
 
-        stream = Runner.run_streamed(
-            agent,
-            message,
-            session=session,
+            You have access to the following tools:
+                - Web Search Tool: Use this when the user asks a questions that isn't in your training data. Use this tool when the users asks about current or future events, when you think you don't know the answer, try searching for it in the web first.
+                - File Search Tool: Use this tool when the user asks a question about facts related to themselves. Or when they ask questions about specific files.
+                - Code Interpreter Tool: Use this tool when you need to write and run code to answer the user's question.
+            """,
+            tools=[
+                WebSearchTool(),
+                FileSearchTool(
+                    vector_store_ids=[VECTOR_STORE_ID],
+                    max_num_results=3,  # 파일이 여러개일 경우 상위 3개만 가져옴.
+                ),
+                ImageGenerationTool(
+                    tool_config={
+                        "type": "image_generation",
+                        "quality": "low",
+                        "output_format": "jpeg",
+                        "moderation": "low",
+                        "partial_images": 1,
+                    }
+                ),
+                CodeInterpreterTool(
+                    tool_config={
+                        "type": "code_interpreter",
+                        "container": {
+                            "type": "auto",
+                            "file_ids": [],  # OpenAI에 업로드 했던 파일의 ID (코드에서 접근할 수 있도록 허용)
+                        },
+                    }
+                ),
+                HostedMCPTool(
+                    tool_config={
+                        "server_url": "https://mcp.context7.com/mcp",
+                        "type": "mcp",
+                        "server_label": "Context7",
+                        "server_description": "Use this to get the docs from software projects",
+                        "require_approval": "never",
+                    }
+                ),
+            ],
         )
 
-        async for event in stream.stream_events():
-            if event.type == "raw_response_event":
-                update_status(status_container, event.data.type)
-                if event.data.type == "response.output_text.delta":
-                    response += event.data.delta
-                    text_placeholder.write(response)
-                if event.data.type == "response.code_interpreter_call_code.delta":
-                    code_response += event.data.delta
-                    code_placeholder.code(code_response)  # syntax highlighting
-                elif event.data.type == "response.image_generation_call.partial_image":
-                    image = base64.b64decode(event.data.partial_image_b64)
-                    image_placeholder.image(image)
+        with st.chat_message("ai"):
+            status_container = st.status("⏳", expanded=False)
+            text_placeholder = st.empty()
+            image_placeholder = st.empty()
+            code_placeholder = st.empty()
+            response = ""
+            code_response = ""
+
+            st.session_state["code_placeholder"] = code_placeholder
+            st.session_state["image_placeholder"] = image_placeholder
+            st.session_state["text_placeholder"] = text_placeholder
+
+            stream = Runner.run_streamed(
+                agent,
+                message,
+                session=session,
+            )
+
+            async for event in stream.stream_events():
+                if event.type == "raw_response_event":
+                    update_status(status_container, event.data.type)
+                    if event.data.type == "response.output_text.delta":
+                        response += event.data.delta
+                        text_placeholder.write(response)
+                    if event.data.type == "response.code_interpreter_call_code.delta":
+                        code_response += event.data.delta
+                        code_placeholder.code(code_response)  # syntax highlighting
+                    elif (
+                        event.data.type
+                        == "response.image_generation_call.partial_image"
+                    ):
+                        image = base64.b64decode(event.data.partial_image_b64)
+                        image_placeholder.image(image)
 
 
 prompt = st.chat_input(
