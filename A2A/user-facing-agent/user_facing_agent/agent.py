d1a593a203d95f282eca8ef70f4ebaac1341ec7d
SeongYun Cho <rekafekili@gmail.com>
2026-04-07 08:55:09 +0900

    #20.3 RemoteA2aAgent

--- A2A/user-facing-agent/user_facing_agent/agent.py
+++ A2A/user-facing-agent/user_facing_agent/agent.py
@@ -4,10 +4,20 @@ load_dotenv()
 
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.agents.remote_a2a_agent import (
+    RemoteA2aAgent,
+    AGENT_CARD_WELL_KNOWN_PATH,
+)
+
+history_agent = RemoteA2aAgent(
+    name="HistoryHelperAgent",
+    description="An agent that can help students with their history homework.",
+    agent_card=f"http://127.0.0.1:8001{AGENT_CARD_WELL_KNOWN_PATH}",
+)
 
 root_agent = Agent(
     name="StudentHelperAgent",
     description="An agent that can help students with their homework.",
     model=LiteLlm("openai/gpt-4o"),
-    sub_agents=[],
+    sub_agents=[history_agent],
 )
