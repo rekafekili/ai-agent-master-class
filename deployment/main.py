3577a57f4849a1ff3696b54d67aafe7fb4b01663
SeongYun Cho <rekafekili@gmail.com>
2026-04-16 00:09:07 +0900

    #21.1 Conversations API

--- deployment/main.py
+++ deployment/main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from deployment!")
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from openai import AsyncOpenAI
+from pydantic import BaseModel
+
+load_dotenv()
+
+from agents import Agent, Runner
+
+agent = Agent(name="Assistant", instructions="You help users with their questions.")
+
+# result = Runner.run_sync(agent, "Why is it called Budapest")
+# print(result.final_output)
+
+app = FastAPI()
+
+client = AsyncOpenAI()
 
 
-if __name__ == "__main__":
-    main()
+class CreateConversationResponse(BaseModel):
+    conversation_id: str
+
+
+@app.post("/conversations")
+async def create_conversation() -> CreateConversationResponse:
+    conversation = await client.conversations.create()
+    return {"conversation_id": conversation.id}
+
+
+@app.post("/conversations/{conversation_id}/message")
+async def create_message(conversation_id: str):
+    pass
